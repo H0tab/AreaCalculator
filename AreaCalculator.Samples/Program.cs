536cb8e047df56e60079395ca0e971f5abba2014
H0tab <72672814+H0tab@users.noreply.github.com>
2022-12-10 20:18:37 +0300

    Delete AreaCalculator.Samples directory

--- AreaCalculator.Samples/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using AreaCalculator.Core;
-
-Triangle triangle1 = new(7, 15, 20);
-Triangle triangle2 = new(13, 30, 42);
-Circle circle1 = new(2);
-Circle circle2 = new(10);
-
-Console.WriteLine(Calculator.CalculateArea(triangle1));
