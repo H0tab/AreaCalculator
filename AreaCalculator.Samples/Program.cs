a1989873b7dd6a46369334afa562bc21e3d56344
H0tab <ksasha18@mail.ru>
2022-12-10 20:16:53 +0300

    Deleted samples

--- AreaCalculator.Samples/Program.cs
+++ AreaCalculator.Samples/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AreaCalculator.Core;
+
+Triangle triangle1 = new(7, 15, 20);
+Triangle triangle2 = new(13, 30, 42);
+Circle circle1 = new(2);
+Circle circle2 = new(10);
+
+Console.WriteLine(Calculator.CalculateArea(triangle1));
